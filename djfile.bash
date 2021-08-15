9a6a84b08ca61f61f68330960d0ff3d799182799
Dingjiang Zhou <dingjiang.zhou@berkshiregrey.com>
2021-08-15 17:10:35 -0400

    add djfile.bash

---
+++ djfile.bash
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# =============================================================================
+function djfile() {
+    cur_dir=${PWD}
+
+    # ------------------------------
+    if [ $# -eq 0 ]; then
+        echo "this is to make djfile program easier to use"
+        return
+    fi
+
+    # ------------------------------
+    if [ $1 = '--cmake' ]; then
+        dj-file --cmake
+        return
+    fi
+
+    # ------------------------------
+    if [ $1 = '--help' ]; then
+        dj-file --help
+        return
+    fi
+    # ------------------------------
+    if [ $1 = '--makefile' ]; then
+        dj-file --makefile
+        return
+    fi
+    # ------------------------------
+    if [ $1 = '--meson' ]; then
+        dj-file --meson
+        return
+    fi
+
+    echo -e 'djfile: "'$1 '"command not supported'
+    djfile_help
+
+    # ------------------------------
+    cd $cur_dir
+    unset cur_dir
+}
+
+# =============================================================================
+function _djfile() {
+    COMPREPLY=()
+
+    # All possible first values in command line
+    local SERVICES=("
+        --cmake
+        --help
+        --makefile
+        --meson
+    ")
+
+    # declare an associative array for options
+    declare -A ACTIONS
+
+    # ------------------------------------------------------------------------
+    ACTIONS["--cmake"]=" "
+    ACTIONS["--help"]=" "
+    ACTIONS["--makefile"]=" "
+    ACTIONS["--meson"]=" "
+
+    # ------------------------------------------------------------------------
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    if [ ${ACTIONS[$3]+1} ]; then
+        COMPREPLY=($(compgen -W "${ACTIONS[$3]}" -- $cur))
+    else
+        COMPREPLY=($(compgen -W "${SERVICES[*]}" -- $cur))
+    fi
+}
+
+# =============================================================================
+complete -F _djfile djfile
