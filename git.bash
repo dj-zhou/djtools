9b73bf69153bb61996b4ca1466225469d4f5ad2d
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-10-19 22:48:44 -0400

    dj git see: update the functions to make the result more neat

--- git.bash
+++ git.bash
@@ -15,38 +15,41 @@ function _dj_git_config() { # name, email
 }
 
 # =============================================================================
+function _dj_git_see_show_result() {
+    while IFS= read -r line; do
+        # only shows the remote branches
+        if [[ "$line" = *"remotes"* ]]; then
+            # to align the date, e.g., to revise "Thu Sep 9 xx:xx:xx" to "Thu Sep 09 xx:xx:xx"
+            if [ "${line:9:1}" = " " ]; then
+                newline="${line:0:7} 0"
+                for i in $(seq 8 ${#line}); do
+                    newline="${newline}${line:${i}:1}"
+                done
+                echo "$newline"
+            else
+                echo "$line"
+            fi
+        fi
+    done <<<"$1"
+}
+
+# =============================================================================
 # options:
 #   1. --name
 #   2. --email
 function _dj_git_see() {
     if [ $# -le 1 ]; then
-        echo "usage: dj git see -name/-email"
+        echo "usage: dj git see -name/-email [name/email]"
         return
     fi
     if [ $1 = '-name' ]; then
         all_branches=$(git for-each-ref --format='%(committerdate) | %(authorname) | %(refname)' | grep "$2")
-        total_lines=$(echo -e "$all_branches" | wc -l)
-
-        while IFS= read -r line; do
-            if [[ "$line" = *"remotes"* ]]; then
-                if [ "${line:9:1}" = " " ]; then
-                    newline="${line:0:7} 0"
-                    for i in $(seq 8 ${#line}); do
-                        newline="${newline}${line:${i}:1}"
-                    done
-                    echo "$newline"
-                else
-                    echo "$line"
-                fi
-            fi
-        done <<<"$all_branches"
+        _dj_git_see_show_result "$all_branches"
+        return
     fi
     if [ $1 = '-email' ]; then
-        git for-each-ref --format='%(committerdate) %09 | %(authoremail) | %(refname)' | grep "$2"
+        all_branches=$(git for-each-ref --format='%(committerdate) | %(authoremail) | %(refname)' | grep "$2")
+        _dj_git_see_show_result "$all_branches"
         return
     fi
 }
-
-# git for-each-ref --sort=committerdate --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
-# Thu Oct 7 07:11:14 2021 -0400
-# Thu Oct  07:11:14 2021 -0400
