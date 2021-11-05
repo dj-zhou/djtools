2affce63f151d564691550a869b5643d714125a6
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-11-05 11:10:49 -0400

    dj python3: install matplotlib

--- python3-tools.bash
+++ python3-tools.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 # variables used in dj-commands.bash ----------
-python3_cmds="pip-install "
-pip_install_list="numpy pandas "
+_python3_cmds="install "
+_install_list="matplotlib numpy pandas "
 
-function _dj_python3_pip_install() {
+function _dj_python3_install() {
+    if [ "$1" = "matplotlib" ]; then
+        sudo apt-get install python3-matplotlib
+        return
+    fi
     if [ "$1" = "numpy" ]; then
         pip3 install numpy
         return
@@ -15,9 +19,9 @@ function _dj_python3_pip_install() {
 }
 
 function _dj_python3() {
-    if [ "$1" = "pip-install" ]; then
+    if [ "$1" = "install" ]; then
         shift 1
-        _dj_python3_pip_install "$@"
+        _dj_python3_install "$@"
         return
     fi
 }
