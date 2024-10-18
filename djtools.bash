092f7ae130dd711ee115ae2174357c553adc898e
Dingjiang Zhou <zhoudingjiang@gmail.com>
2024-10-17 23:31:34 -0400

    wip

--- djtools.bash
+++ djtools.bash
@@ -8,12 +8,21 @@ elif [ $system = 'Linux' ]; then
     rc_file="$HOME/.bashrc"
     # add some global variables -- only tested on Ubuntu
     ubuntu_v=$(lsb_release -a)
-    case $ubuntu_v in
-    "18.04") ubuntu_codename="bionic" ;;
-    "20.04") ubuntu_codename="focal" ;;
-    "22.04") ubuntu_codename="jammy" ;;
-    "24.04") ubuntu_codename="noble" ;;
-    esac
+    # case $ubuntu_v in
+    # "18.04") ubuntu_codename="bionic" ;;
+    # "20.04") ubuntu_codename="focal" ;;
+    # "22.04") ubuntu_codename="jammy" ;;
+    # "24.04") ubuntu_codename="noble" ;;
+    # esac
+    if [[ "${ubuntu_v}" = *'18.04'* ]]; then
+        ubuntu_codename="bionic"
+    elif [[ "${ubuntu_v}" = *'20.04'* ]]; then
+        ubuntu_codename="focal"
+    elif [[ "${ubuntu_v}" = *'22.04'* ]]; then
+        ubuntu_codename="jammy"
+    elif [[ "${ubuntu_v}" = *'24.04'* ]]; then
+        ubuntu_codename="noble"
+    fi
 fi
 
 echo "ubuntu_v=$ubuntu_v"
